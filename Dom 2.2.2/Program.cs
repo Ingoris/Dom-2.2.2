12a857a54b92687bb40b6c1f636ebd30c4cd9846
Андрей Лещенко <Leshenko.a.v.99@gmail.com>
2019-12-02 22:43:31 +0200

    Добавьте файлы проекта.

---
+++ Dom 2.2.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dom_2._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Название товара: ");
+            string name = Console.ReadLine();
+            Console.Write("Количество : ");
+            double quantity = double.Parse(Console.ReadLine().Replace(".", ","));
+            Console.Write("Единицы измерения (кг): ");
+            string number = Console.ReadLine();
+            Console.Write("Цена : ");
+            double price = double.Parse(Console.ReadLine().Replace(".", ","));
+            Console.WriteLine("Название товара {0}, \nКоличество  {1}, \nЕдиницы измерения (кг)  {2}, \nЦена {3}", name.ToUpper(), quantity, number.ToLower(), price);
+            Console.ReadKey(); 
+        }
+    }
+}
